8e60d30659b99b1277e8133c88920d4e52c9bf2f
Amina Shojaei <aminashojaeii@gmail.com>
2025-06-30 03:30:50 +0330

    insert CreateView, UpdateView and DeleteView

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
     model = Task
-# Create your views here.
+    context_object_name = 'tasks'
+    template_name = 'todo_list/task_list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todo_list/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['title', 'description']
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description']
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    template_name = 'todo_list/task_confirm_delete.html'
